a41e51e2a1e5633e2bd40a2f1f97f68503282b22
Basuco <alejandro.g.206@gmail.com>
2014-11-06 01:42:04 -0430

    Se tienen que crear una base de datos en postgre llamada sage_development, que tenga owner sage, de clave sage

--- src/SAGE/config/settings.py
+++ src/SAGE/config/settings.py
@@ -65,7 +65,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sage_development',
+        'USER': 'sage',
+        'PASSWORD': 'sage',
+        'HOST': '127.0.0.1',
+        #'PORT': '5432',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
