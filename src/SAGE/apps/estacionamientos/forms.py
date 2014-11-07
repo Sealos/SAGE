de978a48ffc7160e0e8e2c13ae1b1ebcc49ba1bd
Gabriela <gabrielagimenez13@hotmail.com>
2014-11-07 00:13:45 -0430

    Cambie algunos detalles de la interfaz

--- src/SAGE/apps/estacionamientos/forms.py
+++ src/SAGE/apps/estacionamientos/forms.py
@@ -48,10 +48,10 @@ class EstacionamientoForm(forms.Form):
 class EstacionamientoExtendedForm(forms.Form):
 
 
-    puestos = forms.IntegerField(min_value=0, label='Número de puestos')
+    puestos = forms.IntegerField(min_value=0, label='Número de Puestos')
 
     tarifa_validator = RegexValidator(
-                            regex='^[0-9]+$',
+                            regex='^(([0-9])|([0-9]*.[0-9]*))+$',
                             message='Sólo debe contener dígitos.'
                         )
 
